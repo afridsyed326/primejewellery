d32609ee152cba6d4b17606b24283f8baf4fc959
afridsyed326 <afrid@utopianglobal.com>
2023-11-05 14:13:01 +0400

    Initial Commit

---
+++ primejewellery/primejewelleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  primejewelleryApp.swift
+//  primejewellery
+//
+//  Created by S M Afrid on 05/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct primejewelleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
